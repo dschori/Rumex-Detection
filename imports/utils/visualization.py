06fd13624e3ce3659f14d6c70771311f9be271c1
dschori <37111668+dschori@users.noreply.github.com>
2019-03-09 16:41:30 +0100

    Changed Visualization Class to read df

--- imports/utils/visualization.py
+++ imports/utils/visualization.py
@@ -10,23 +10,22 @@ from skimage import img_as_float
 from imports.utils.enums import DATA_BASE_PATH, SHAPE
 from imports.utils.log_progress import log_progress
 
+# https://stackoverflow.com/questions/9056957/correct-way-to-define-class-variables-in-python
 
 class Visualize():
-    def __init__(self,data_set,model):
+    def __init__(self,df,data_set,model):
+        self.df = df
         self.data_set = data_set
         self.model = model
         self.figsize = (10,10)
         self.prediction_threshold = 0.95
         self.index = None
-        self.index_loaded = None
+        self.df_row_at_index = None
         self.mode = None
         self.img = None
         self.msk = None
         self.prediction = None
-        self.indices = None
         self.dice_score = None
-        self.directory = self.__get_directory()
-        self.file_list = os.listdir(self.directory+'images')
 
     def get_data_set(self):
         return self.data_set
@@ -92,8 +91,7 @@ class Visualize():
             index = []
             n = rows*2
             for i in range(n):
-                tmp_index = np.random.random_integers(0,len(self.file_list))
-                tmp_index = self.file_list[tmp_index]
+                tmp_index = np.random.random_integers(0,len(self.df))
                 index.append(tmp_index)
         else:
             n = len(index)
@@ -107,6 +105,10 @@ class Visualize():
             ind = index[2*i:2*i+2]
             self.__make_image_row(ind,ax[i]) 
         plt.subplots_adjust(wspace=0.01, hspace=0)
+                         
+    def __make_image_row(self,index,ax):
+        self.__add_image(index[0],ax[0])
+        self.__add_image(index[1],ax[1])
             
     def __add_image(self,index,ax=None):
         '''
@@ -114,15 +116,14 @@ class Visualize():
         '''
         
         if index == 'random':
-            self.index = np.random.random_integers(0,len(self.file_list))
-            self.index = self.file_list[self.index]
+            self.index = np.random.random_integers(0,len(self.df))
         else:
             self.index = index
             
         self.__load_data()
         
         if ax == None:
-            fig, ax = plt.subplots(figsize=self.figsize)
+            _, ax = plt.subplots(figsize=self.figsize)
         if self.mode == "image":
             ax.imshow(self.img)
         if self.mode == "mask":
@@ -145,35 +146,21 @@ class Visualize():
             ax.imshow(self.error,cmap='Reds', alpha=0.4, interpolation='none')
         if self.dice_score == None:
             self.dice_score = 0.0
-        ax.set_title('Image Nr: ' + str(self.file_list[self.indices[0]]) + "  Dice Coeff: " + str(round(self.dice_score,2)), fontsize=15)
+        ax.set_title('Image: ' + str(self.df_row_at_index.name.values[0]) + "  Dice Coeff: " + str(round(self.dice_score,2)), fontsize=15)
         ax.axis('off')
         self.dice_score = None
-                         
-    def __make_image_row(self,index,ax):
-        self.__add_image(index[0],ax[0])
-        self.__add_image(index[1],ax[1])
-        
-    def __get_directory(self):
-        if self.data_set == "train":
-            directory = DATA_BASE_PATH+"/01_train/"
-        elif self.data_set == "augmented":
-            directory = DATA_BASE_PATH+"/02_augmented/"
-        elif self.data_set == "val":
-            directory = DATA_BASE_PATH+"/03_val/"
-        elif self.data_set == "test":
-            directory = DATA_BASE_PATH+"/04_test/"
-        else:
-            raise ValueError('Invalid "data_set"')
-        return directory
         
     def __load_data(self):
-        self.indices = [i for i, s in enumerate(self.file_list) if str(self.index) in s]
+
+        self.df_row_at_index = self.df[self.df['name'].str.contains(str(self.index))]
         
-        if len(self.indices) == 0 or len(self.indices) > 1:
+        if len(self.df_row_at_index) == 0:
             raise ValueError('Image not found')
+        elif len(self.df_row_at_index) > 1:
+            raise ValueError('Index not unique')
         
-        img= imread(self.directory+"images/"+self.file_list[self.indices[0]])
-        msk = imread(self.directory+"masks/"+self.file_list[self.indices[0]])
+        img= imread(self.df_row_at_index.image_path.values[0]+self.df_row_at_index.name.values[0])
+        msk = imread(self.df_row_at_index.mask_path.values[0]+self.df_row_at_index.name.values[0])
         
         self.img = resize(img,(SHAPE[0],SHAPE[1])).reshape(*SHAPE,3)
         self.img = img_as_float(self.img)
@@ -195,3 +182,24 @@ class Visualize():
         y_pred_f = np.ndarray.flatten(self.prediction.astype(float))
         intersection = np.sum(y_true_f * y_pred_f)
         self.dice_score = (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
+
+class Evaluate(Visualize):
+    '''
+    Evaluate specific Model
+    Target: tbd
+    '''
+    def __init__(self,df,model):
+        self.df = df
+        self.img = None
+
+    def __find_roots(self):
+        pass
+
+    def __eval_circle_model(self):
+        for _, row in self.df.iterrows():
+            self.img = imread(row['image_path']+row['name'])
+            self.__predict()
+            self.__find_roots()
+            roots = row['roots']
+            for root in roots:
+                pass
